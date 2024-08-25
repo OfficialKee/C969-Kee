70c10e464c34237dc95172f8b01611eb75935ed0
shike <shike@KEE>
2024-08-25 15:29:19 -0400

    Add project files.

---
+++ C969-Kee/Forms/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace C969_Kee
+{
+    partial class loginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkB = new System.Windows.Forms.Button();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkB
+            // 
+            this.checkB.Location = new System.Drawing.Point(291, 374);
+            this.checkB.Name = "checkB";
+            this.checkB.Size = new System.Drawing.Size(139, 23);
+            this.checkB.TabIndex = 1;
+            this.checkB.Text = "Check Connection";
+            this.checkB.UseVisualStyleBackColor = true;
+            this.checkB.Click += new System.EventHandler(this.checkB_Click);
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.Location = new System.Drawing.Point(337, 28);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(108, 25);
+            this.welcomeLabel.TabIndex = 4;
+            this.welcomeLabel.Text = "Welcome";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.Location = new System.Drawing.Point(214, 96);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(85, 16);
+            this.usernameLabel.TabIndex = 5;
+            this.usernameLabel.Text = "User Name";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(224, 147);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(75, 16);
+            this.passwordLabel.TabIndex = 6;
+            this.passwordLabel.Text = "Password";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(305, 96);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(165, 20);
+            this.usernameTextBox.TabIndex = 7;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(305, 147);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(165, 20);
+            this.passwordTextBox.TabIndex = 8;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(342, 215);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 9;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // loginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.welcomeLabel);
+            this.Controls.Add(this.checkB);
+            this.Name = "loginForm";
+            this.Text = "Login Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button checkB;
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
---
+++ C969-Kee/Forms/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using C969_Kee.Database;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Math.Field;
+
+namespace C969_Kee
+{
+    public partial class loginForm : Form
+    {
+        public MySqlCommand command;
+        public MySqlDataReader dr;
+        public loginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void checkB_Click(object sender, EventArgs e)
+        {
+            MySqlConnection c = DBConnection.Conn;
+
+            if (c.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Connection Open");
+            }
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string userName, password;
+
+            userName = usernameTextBox.Text;
+            password = passwordTextBox.Text;
+
+            try
+            {
+                if (DBConnection.Conn.State != ConnectionState.Open)
+                {
+                    DBConnection.startConnection();
+                }
+                
+                String selectQuery = "SELECT * FROM user Where userName = '"+userName+"' AND password = '"+password+"'";
+                command = new MySqlCommand(selectQuery, DBConnection.Conn);
+                command.Parameters.AddWithValue("userName", userName);
+                command.Parameters.AddWithValue("@password", password);
+                dr = command.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    MessageBox.Show("Login Successful", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Hide();
+                    string LoggedUserName = userName;
+                    //DashboardForm dashboardForm = new DashboardForm(LoggedUserName);
+                    //dashboardForm.Show();
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Invalid login details: 'Error' ");
+                    //usernameTextBox.Clear();
+                    //passwordTextBox.Clear();
+                }
+                
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                DBConnection.closeConnection();
+                //MessageBox.Show("finally");
+            }
+        }
+    }
+}
